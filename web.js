260090a3c62507b24402d7b3be29c7a6451b7d06
Kevin Scott <kevin.scott@venmo.com>
2013-11-01 17:32:12 -0400

    more shit

--- web.js
+++ web.js
@@ -2,6 +2,24 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-east-04.cleardb.com',
+  user     : 'b6204b17cbb96d',
+  password : '9a9cd260',
+  database : 'heroku_b378eaa431255a3'
+});
+
+connection.connect();
+
+connection.query('INSERT INTO users (fbid, created) VALUES ("123", NOW())', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log(rows);
+});
+
+connection.end();
+
 app.get('/', function(request, response) {
     response.send('Hello World!');
 });
@@ -9,4 +27,5 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
+
 });
