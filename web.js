4634cd048d701db944cd3199421b9da195d40c07
Kevin Scott <kevin@scottlabsllc.com>
2013-11-02 19:26:37 -0400

    Killing it, and added thresholds

--- web.js
+++ web.js
@@ -8,13 +8,14 @@ app.use(express.bodyParser());
 
 
 var mysql      = require('mysql');
-
-var connection = mysql.createConnection({
+var db_config = {
 	  host     : 'us-cdbr-east-04.cleardb.com',
 	  user     : 'b6204b17cbb96d',
 	  password : '9a9cd260',
 	  database : 'heroku_b378eaa431255a3'
-	});
+	};
+
+var connection = mysql.createConnection(db_config);
 
 
 
@@ -55,22 +56,37 @@ var query = function(query,callback,error) {
 	});
 }
 
+
 var createUser = function(uuid, callback) {
 	var query_string = 'INSERT INTO users (uuid, created) VALUES ('+uuid+', NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
 	query(query_string,callback);
 
 };
 
-var createHandshake = function(user_id,lat,lng,timestamp,callback){
-	var query_string = 'INSERT INTO shakes (user_id, lat, lng, timestamp, created) VALUES ('+user_id+','+lat+','+lng+',FROM_UNIXTIME('+timestamp+'), NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
-	query(query_string,callback);
+var createHandshake = function(user_id,lat,lng,timestamp,location_threshold,timestamp_threshold,callback){
+	var create_handshake = 'INSERT INTO shakes (user_id, lat, lng, timestamp, created) VALUES ('+user_id+','+lat+','+lng+',FROM_UNIXTIME('+timestamp+'), NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
+	query(create_handshake,function(rows){
+
+		var grab_partner = '	SELECT *, \
+								ABS(UNIX_TIMESTAMP(timestamp)-'+timestamp+'), ABS(lat-'+lat+'), ABS(lng-'+lng+') \
+								FROM shakes WHERE 1=1 \
+									AND timestamp >= DATE_SUB(FROM_UNIXTIME('+timestamp+'),INTERVAL '+timestamp_threshold+' SECOND) \
+									AND timestamp <= DATE_ADD(FROM_UNIXTIME('+timestamp+'),INTERVAL '+timestamp_threshold+' SECOND) \
+									AND lat >= '+lat+'-'+location_threshold+' \
+									AND lat <= '+lat+'+'+location_threshold+' \
+									AND lng >= '+lng+'-'+location_threshold+' \
+									AND lng <= '+lng+'+'+location_threshold+' \
+									AND user_id != '+user_id+' \
+							';
+		query(grab_partner,callback);
+	});
 };
 
 var createAccessToken = function(user_id,access_token,callback){
 	var query_string = 'INSERT INTO access_tokens (user_id, access_token, created) VALUES ('+user_id+','+access_token+', NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
 	query(query_string,callback);
 };
-app.post('/shakes/add', function(request, response) {
+app.post('/shakes/add', function(request, res) {
 
 
 
@@ -78,16 +94,17 @@ app.post('/shakes/add', function(request, response) {
 	var uuid = connection.escape(request.body.uuid);
 	var lat = connection.escape(request.body.lat);
 	var lng = connection.escape(request.body.lng);
-	var timestamp = connection.escape(request.body.time);
+	var timestamp = connection.escape(request.body.timestamp);
+
+	var location_threshold = connection.escape(request.body.location_threshold || 0.0005);
+	var timestamp_threshold = connection.escape(request.body.timestamp_threshold || 20);
 
 	createUser(uuid, function(rows){
 		var user_id = rows.insertId;
 		createAccessToken(user_id,access_token,function(rows){
-			createHandshake(user_id,lat,lng,timestamp,function(rows){
+			createHandshake(user_id,lat,lng,timestamp,location_threshold,timestamp_threshold,function(rows){
 				console.log('rows',rows);
-				res.json({ msgId: msg.fileName })
-
-				response.send('Good job.');
+				res.json({ rows: rows })
 			});
 		});
 	});
@@ -102,3 +119,30 @@ app.listen(port, function() {
     console.log("Listening on " + port);
 
 });
+
+
+
+
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
