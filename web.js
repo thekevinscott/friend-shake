69412f98c5afa96067c3054168df1f9b5375c21d
Kevin Scott <kevin@scottlabsllc.com>
2013-11-06 09:44:52 -0500

    incorporates promises and heavily cleans up code

--- web.js
+++ web.js
@@ -23,29 +23,31 @@ var db_config = {
 
 var pool = {};
 
-var connection = mysql.createConnection(db_config);
-
 
 
 
 
 app.get('/', function(request, response) {
-
 	response.send('What are you doing here? Go away.');
 });
 app.get('/jquery', function(request, response) {
-          response.send('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>');
+    response.send('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>');
 });
+
+
+
 app.get('/meetings/*', function(request, response) {
-	var uuid = connection.escape(request.params[0]);
+
+	var uuid = request.params[0];
 
 	var meetings_query = "SELECT s.timestamp, u.username, u.fbid, u.uuid, u.firstname FROM (SELECT m.created as timestamp, m.user_b as target_user \
 							FROM meetings m \
 							LEFT JOIN users u ON u.id = m.user_a \
-							WHERE u.uuid = "+uuid+" \
+							WHERE u.uuid = ? \
 							) s LEFT JOIN users u ON u.id = s.target_user ";
-console.log(meetings_query);
-	query(meetings_query, function(rows){
+
+
+	pool.query(meetings_query,[uuid]).then(function(rows){
 		if (rows.length) {
 			response.json({meetings : rows});
 		} else {
@@ -56,49 +58,49 @@ console.log(meetings_query);
 });
 
 
-var query = function(query,callback,error) {
-console.log('query',query);
-
-	connection.query(query, function(err, rows, fields) {
-		if (err) {
-			if (error) { error(err);}
-			else { console.log('query',query); console.log('err',err); }
-		} else {
-
-			if (callback) { callback(rows); }
-		}
-	});
-}
 
 
 var createUser = function(params, callback, error_callback) {
-	var query_string = 'INSERT INTO users (uuid, fbid, username, firstname, created) VALUES ('+params.uuid+', '+params.fbid+','+params.username+','+params.firstname+',NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id), uuid='+params.uuid+',fbid='+params.fbid+',username='+params.username;
-	query(query_string,callback,error_callback);
+
+
+	var query_string = 'INSERT INTO users (uuid, fbid, username, firstname, created) \
+						VALUES (?,?,?,?,NOW()) \
+						ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id), uuid=?,fbid=?,username=?,firstname=?';
+	params = [params.uuid,params.fbid,params.username,params.firstname,params.uuid,params.fbid,params.username,params.firstname];
+	pool.query(query_string,params).then(callback).fin(error_callback);
 
 };
 
 var createHandshake = function(user_id,params,callback,error_callback){
-	var create_handshake = 'INSERT INTO shakes (user_id, lat, lng, timestamp, created) VALUES ('+user_id+','+params.lat+','+params.lng+',FROM_UNIXTIME('+params.timestamp+'), NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
 
-	query(create_handshake,function(rows){
+	var handshake_params = [user_id,params.lat,params.lng,params.timestamp];
+
+
+
+	var create_handshake = 'INSERT INTO shakes (user_id, lat, lng, timestamp, created) VALUES (?,?,?,FROM_UNIXTIME(?), NOW()) \
+							ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
+
+
+	pool.query(create_handshake,handshake_params).then(function(rows){
 
 		params.timestamp_threshold /= 2;
 		params.location_threshold /= 2;
 
-		var lat = parseFloat(params.lat.replace(/'/g,''));
-		var lng = parseFloat(params.lng.replace(/'/g,''));
+		var partner_params = [	params.timestamp, params.lat, params.lng,
+								params.timestamp, params.timestamp_threshold, params.timestamp, params.timestamp_threshold,
+								user_id];
 
-
-		var grab_partner = '	SELECT shakes.*, u.fbid, u.username, u.firstname, \
-								ABS(UNIX_TIMESTAMP(timestamp)-'+params.timestamp+') as timestamp_difference, \
-								ABS(lat-'+params.lat+') as lat_distance, \
-								ABS(lng-'+params.lng+') as lng_distance \
+		var grab_partner = '	SELECT shakes.*, u.fbid, u.username, u.firstname, a.access_token, \
+								ABS(UNIX_TIMESTAMP(timestamp)-?) as timestamp_difference, \
+								ABS(lat-?) as lat_distance, \
+								ABS(lng-?) as lng_distance \
 								FROM shakes \
 								LEFT JOIN users u ON u.id = shakes.user_id \
+								LEFT JOIN access_tokens a ON a.user_id = u.user_id \
 								WHERE 1=1 \
-									AND timestamp >= DATE_SUB(FROM_UNIXTIME('+params.timestamp+'),INTERVAL '+params.timestamp_threshold+' SECOND) \
-									AND timestamp <= DATE_ADD(FROM_UNIXTIME('+params.timestamp+'),INTERVAL '+params.timestamp_threshold+' SECOND) \
-									AND user_id != '+user_id+' ';
+									AND timestamp >= DATE_SUB(FROM_UNIXTIME(?),INTERVAL ? SECOND) \
+									AND timestamp <= DATE_ADD(FROM_UNIXTIME(?),INTERVAL ? SECOND) \
+									AND user_id != ? ';
 /*
 		var grab_partner = '	SELECT shakes.*, u.fbid, u.username, u.firstname, \
 								ABS(UNIX_TIMESTAMP(timestamp)-'+params.timestamp+') as timestamp_difference, \
@@ -115,13 +117,17 @@ var createHandshake = function(user_id,params,callback,error_callback){
 									AND lng <= '+(lng + params.location_threshold)+' \
 									AND user_id != '+user_id+' ';
 									*/
-		query(grab_partner,callback,error_callback);
-	},error_callback);
+		pool.query(grab_partner,partners_params).then(callback).fin(error_callback);
+	}).fin(error_callback);
+
+
 };
 
 var createAccessToken = function(user_id,access_token,callback,error_callback){
-	var query_string = 'INSERT INTO access_tokens (user_id, access_token, created) VALUES ('+user_id+','+access_token+', NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
-	query(query_string,callback,error_callback);
+
+	var query_string = 'INSERT INTO access_tokens (user_id, access_token, created) VALUES (?,?, NOW()) ON DUPLICATE KEY UPDATE id= LAST_INSERT_ID(id)';
+	pool.query(query_string,[user_id,access_token]).then(callback).fin(error_callback);
+
 };
 
 var sendMeetRequest = function(row, callback) {
@@ -221,6 +227,9 @@ curl -X POST https://graph.facebook.com/me/shakepebble:meet
 
 }
 
+
+
+
 app.post('/shakes/add', function(request, res) {
 
 	var params = {};
@@ -232,113 +241,50 @@ app.post('/shakes/add', function(request, res) {
 			res.json({error : "Missing field " + require});
 			return;
 		}
-		params[require] = connection.escape(request.body[require]);
+		params[require] = request.body[require];
 	}
+
 	console.log("params",params);
 
 	params.location_threshold = parseFloat(request.body.location_threshold || 0.0005);
 	params.timestamp_threshold = parseFloat(request.body.timestamp_threshold || 10);
 
+
 	createUser(params, function(rows){
-		var user_id = rows.insertId;
-		var username = params.username;
-		createAccessToken(user_id,params.access_token,function(rows){
-			createHandshake(user_id,params,function(rows){
+		var user = {
+			id: rows.insertId,
+			username: params.username,
+			firstname: params.firstname,
+			fbid: params.fbid
+		};
 
+		createAccessToken(user.id,params.access_token,function(rows){
+			user.access_token = params.access_token;
+			createHandshake(user.id,params,function(rows){
 
-				var pebble_response = {};
 
 				if (rows.length) {
 					console.log('rows were found, woot woot',rows);
+					var row = rows[0];
 
-					// have we already made a meet request?
-					var meeting_query = "SELECT * FROM meetings \
-										WHERE created > DATE_SUB(NOW(), INTERVAL 1 MINUTE) \
-										AND user_a = "+user_id+" \
-										AND user_b = "+rows[0]['user_id']+" ";
+					var friend = {
+						id: row.user_id,
+						username: row.username,
+						firstname: row.firstname,
+						fbid: row.fbid,
+						access_token: row.access_token
+					};
 
-					query(meeting_query,function(meeting_rows){
-						if (! meeting_rows.length) {
-							console.log("**** go ahead and make the meeting: "+username.replace(/'/g,'')+" met "+rows[0]['username']+" ****");
-							query("INSERT INTO meetings (user_a,user_b,created) VALUES ('"+user_id+"','"+rows[0]['user_id']+"',NOW()) ", function(m_rows){
-								var meeting_id = m_rows.insertId;
+					var handshake = {
+						timestamp: row.timestamp,
+						lat: row.lat,
+						lng: row.lng
+					};
 
-								// no meet request has been made, proceed
-
-								var sendResponse = function() {
-									res.json({ rows: rows, pebble_response : pebble_response});
-								}
-
-								var makeMeetRequest = function(params,callback) {
-									console.log('params for meet request', params);
-									sendMeetRequest(params,function(data){
-										pebble_response[params.me.replace(/'/g,'')] = data;
-										//console.log("do callback");
-										callback();
-									});
-								}
-
-								console.log('params at the top',params);
-
-								makeMeetRequest({
-									target_username : rows[0].username, // their username
-									target_firstname : rows[0].firstname, // their firstname
-									access_token : params.access_token, // my access token
-									me : params.username, // me
-									firstname : params.firstname,
-									fbid : params.fbid
-								},function(){
-									// here we should check if we need to make a second query
-									var second_meeting_query = "SELECT * FROM meetings \
-														WHERE created > DATE_SUB(NOW(), INTERVAL 1 MINUTE) \
-														AND user_b = "+user_id+" \
-														AND user_a = "+rows[0]['user_id']+" ";
-									query(second_meeting_query,function(second_meeting_rows) {
-										if (! second_meeting_rows.length) {
-											console.log("**** go ahead and make the meeting: "+rows[0]['username'] +" met "+username.replace(/'/g,'')+" ****");
-											query("INSERT INTO meetings (user_a,user_b,created) VALUES ('"+user_id+"','"+rows[0]['user_id']+"',NOW()) ", function(m_rows){
-												var meeting_id2 = m_rows.insertId;
-												var access_token_query = 'SELECT a.access_token, u.username, u.firstname, u.fbid FROM access_tokens a \
-																			LEFT JOIN users u ON u.id = a.user_id \
-																			WHERE a.user_id = '+rows[0].user_id+' \
-																			ORDER BY a.id DESC ';
-
-												query(access_token_query,function(rows){
-													console.log('rows',rows);
-													if (rows.length) {
-
-														makeMeetRequest({
-															target_username : params.username, // their username
-															target_firstname : params.firstname, // their first name
-															access_token : rows[0].access_token, // my access token
-															me : rows[0].username, // me
-															firstname : rows[0].firstname,
-															fbid : rows[0].fbid
-														},function(){
-															sendResponse();
-														});
-													} else {
-														console.log('What the fuck is this, why are there no rows');
-														console.log(access_token_query);
-														//res.json({error: 'No rows found, what the fuck'});
-													}
-
-												});
-											});
-
-										} else {
-											sendResponse();
-										}
-									});
-								});
-							});
-
-
-/*
-								*/
-						} else {
-							res.json({ message: 'A meeting has already happened between these users within the time threshold.' });
-						}
+					handleMeetingRequest({
+						user: user,
+						friend: friend,
+						handshake : handshake
 					});
 
 
@@ -358,6 +304,99 @@ app.post('/shakes/add', function(request, res) {
 
 });
 
+
+
+
+
+
+var handleMeetingRequest = function(params) {
+	var pebble_response = {};
+	var user = params.user;
+	var friend = params.friend;
+	var handshake = params.handshake;
+
+	// have we already made a meet request?
+	var minute_threshold = 1;
+	var meeting_query = "SELECT * FROM meetings \
+						WHERE created > DATE_SUB(NOW(), INTERVAL "+minute_threshold+" MINUTE) \
+						AND user_a = ? AND user_b = ? ";
+	pool.query(meeting_query,[user.id,friend.id]).then(function(meeting_rows){
+		if (! meeting_rows.length) {
+			console.log("**** go ahead and make the meeting: "+user.username+" met "+friend.username+" ****");
+
+			var meeting_insert_params = [user.id, friend.id];
+			var meeting_insert = "INSERT INTO meetings (user_a,user_b,created) VALUES (?,?,NOW()) ";
+			pool.query(meeting_insert,meeting_insert_params).then(function(m_rows){
+				var meeting_id = m_rows.insertId;
+
+				var sendResponse = function() {
+					res.json({ rows: rows, pebble_response : pebble_response});
+				}
+
+
+				var makeMeetRequest = function(params,callback) {
+					console.log('params for meet request', params);
+					sendMeetRequest(params,function(data){
+						pebble_response[params.me.replace(/'/g,'')] = data;
+						//console.log("do callback");
+						callback();
+					});
+				}
+
+				console.log('params at the top',params);
+
+				makeMeetRequest({
+					target_username : friend.username, // their username
+					target_firstname : friend.firstname, // their firstname
+					access_token : user.access_token, // my access token
+					me : user.username, // me
+					firstname : user.firstname, // my firstname
+					fbid : user.fbid // my fbid
+				},function(){
+					// here we should check if we need to make a second query
+					var second_meeting_params = [friend.id,user.id];
+					var second_meeting_query = "SELECT * FROM meetings \
+										WHERE created > DATE_SUB(NOW(), INTERVAL 1 MINUTE) \
+										AND user_a = ? \
+										AND user_b = ? ";
+					pool.query(second_meeting_query,second_meeting_params).then(function(second_meeting_rows){
+						if (! second_meeting_rows.length) {
+							console.log("**** go ahead and make the meeting: "+friend.username +" met "+user.username+" ****");
+							var meeting_insert_params = [friend.id,user.id];
+							var meeting_insert = "INSERT INTO meetings (user_a,user_b,created) VALUES (?,?,NOW()) ";
+							pool.query(meeting_insert,meeting_insert_params).then(function(m_rows){
+
+								makeMeetRequest({
+									target_username : user.username, // their username
+									target_firstname : user.firstname, // their first name
+									access_token : friend.access_token, // my access token
+									me : friend.username, // me
+									firstname : friend.firstname, // my firstname
+									fbid : friend.fbid // my fbid
+								},function(){
+									sendResponse();
+								});
+
+							});
+						} else {
+							// no meeting needed, it already exists
+							sendResponse();
+						}
+					});
+
+				});
+			});
+
+		} else {
+			res.json({ message: 'A meeting has already happened between these users within the time threshold.' });
+		}
+
+
+	}).fin(function(error){
+		console.log('there was an unknown error',err);
+		res.json({ err: err });
+	});
+};
 
 // delete the below two methods
 // only for testing purposes
@@ -395,6 +434,13 @@ app.get('/meet',function(request,res){
 
 
 
+
+
+
+
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
@@ -410,7 +456,7 @@ app.listen(port, function() {
 
 
 pool = function() {
-
+	var connection;
 	function handleDisconnect() {
 	  connection = mysql.createConnection(db_config); // Recreate the connection, since
 	                                                  // the old one cannot be reused.
@@ -418,13 +464,15 @@ pool = function() {
 	  connection.connect(function(err) {              // The server is either down
 	    if(err) {                                     // or restarting (takes a while sometimes).
 	      console.log('error when connecting to db:', err);
-	      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+	      connection = null;
+	      setTimeout(handleDisconnect, 500); // We introduce a delay before attempting to reconnect,
 	    }                                     // to avoid a hot loop, and to allow our node script to
 	  });                                     // process asynchronous requests in the meantime.
 	                                          // If you're also serving http, display a 503 error.
 	  connection.on('error', function(err) {
 
 	    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+	      connection = null;
 	      handleDisconnect();                         // lost due to either server restart, or a
 	    } else {                                      // connnection idle timeout (the wait_timeout
 	      console.log('db error', err);
@@ -432,24 +480,46 @@ pool = function() {
 	    }
 	  });
 	}
-	var getConnection = function() {
-		return connection;
+
+
+
+	var getConnection = function(deferred) {
+		if (! deferred) { var deferred = q.defer(); }
+		if (connection) {
+			deferred.resolve(connection);
+		} else {
+			setTimeout(function(){
+				getConnection(deferred);
+			},500);
+		}
+		return deferred.promise;
 	}
 
 
 	handleDisconnect();
 
+
+
+	var query = function(query,params) {
+		var deferred = q.defer();
+		getConnection().then(function(conn){
+			conn.query(query, function(err, rows, fields) {
+				if (err) {
+					deferred.reject(err);
+					console.log('Query Error',query); console.log('err',err);
+				} else {
+					deferred.resolve(rows);
+				}
+			});
+		});
+		return deferred.promise;
+	}
+
 	return {
-		getConnection : getConnection
+		getConnection : getConnection,
+		query : query
 	};
 }();
-
-
-
-
-
-
-
 
 
 
